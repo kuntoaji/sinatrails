00f085669e68ba0e8ba519e33d25280a5531f66c
kuntoaji <kunto.aji.kr@gmail.com>
2010-10-07 17:10:13 +0700

    load all files under app/mailers

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application < Sinatra::Base
 end
 
 require File.expand_path('../config/environment.rb', __FILE__)
-%w{app/controllers app/helpers app/models lib}.each do |dir|
+%w{app/controllers app/helpers app/mailers app/models lib}.each do |dir|
   $LOAD_PATH.unshift(dir)
   Dir[File.join(dir, '*.rb')].each{|file| require File.basename(file) }
 end
